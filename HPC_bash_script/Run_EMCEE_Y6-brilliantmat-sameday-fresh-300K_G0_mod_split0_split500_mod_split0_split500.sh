1dc63523ceb2c566a193211443744d7a517aa4ab
hanboy315 <73485481+hanboy315@users.noreply.github.com>
2024-10-30 20:24:49 +0000

    Create Run_EMCEE_Y6-brilliantmat-sameday-fresh-300K_G0_mod_split0_split500_mod_split0_split500.sh

---
+++ HPC_bash_script/Run_EMCEE_Y6-brilliantmat-sameday-fresh-300K_G0_mod_split0_split500_mod_split0_split500.sh
@@ -0,0 +1,8 @@
+#!/bin/bash 
+#PBS -l walltime=70:59:01 
+#PBS -l select=1:ncpus=1:mem=80gb:avx=true 
+ 
+cd /rds/general/user/hy2120/home/pl_temp_fit 
+module load anaconda3/personal 
+source activate pl_temp_fit     
+python src/pl_temp_fit/scripts/new_pl_sampling_60lifetime_abs.py --model_config_id 30751d6d-eaee-41bc-873a-b74ebb69e77b 
